a31ddc8b6e6169eaf25b71374113c4a88efc3e27
Facundo Cristobal <Facundo Cristobal>
2016-06-22 10:30:11 -0300

    prueba heroku

--- js/script.js
+++ js/script.js
@@ -2,14 +2,6 @@
 
 function mostrar(numero){
   for (var i = 0; i < document.getElementsByClassName('prodfiltrado').length; i++){
-     document.getElementsByClassName('prodfiltrado')[i].style.display= 'none';
+    document.getElementsByClassName('prodfiltrado')[i].style.display= 'none';
   }
   document.getElementsByClassName('prodfiltrado')[numero].style.display= 'inline-block';
-}
-
-
-
-$("#barraNav li").click(function(){
-  $(".active").toggleClass("active");
-  $(this).toggleClass("active");
-});
