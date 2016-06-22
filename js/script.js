1df65072a3bb8317e6bd91f25c8522b00fcfd285
Facundo Cristobal <Facundo Cristobal>
2016-06-22 09:19:38 -0300

    close #9, creada home

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 "use strict";
 
+function mostrar(numero){
+  for (var i = 0; i < document.getElementsByClassName('prodfiltrado').length; i++){
+     document.getElementsByClassName('prodfiltrado')[i].style.display= 'none';
+  }
+  document.getElementsByClassName('prodfiltrado')[numero].style.display= 'inline-block';
+}
+
+
+
 $("#barraNav li").click(function(){
   $(".active").toggleClass("active");
   $(this).toggleClass("active");
